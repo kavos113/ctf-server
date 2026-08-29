3b970d0113e548986626781cc6b2d222975ef18f
kavos113 <kohei.hadano@gmail.com>
2026-08-29 14:34:39 +0900

    write buffer by writev

---
+++ src/server_p.h
@@ -0,0 +1,19 @@
+#ifndef SERVER_P_H
+#define SERVER_P_H
+
+#include <stdint.h>
+
+#include "server.h"
+
+void setup_shutdown(server_t *srv);
+
+void listen_handler(const server_t *srv);
+void client_handler(const server_t *srv, connection_t *conn);
+
+int add_connection(const server_t *server, connection_t *conn, uint32_t event_mask);
+void remove_connection(const server_t *server, connection_t *conn);
+
+// 1: success, 0: partial, -1: error
+int connection_send_buffer(connection_t *conn);
+
+#endif // SERVER_P_H
