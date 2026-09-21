228b59e5b2eeeb362aa7b9dd9743f4e5b8dc722b
kavos113 <kohei.hadano@gmail.com>
2026-09-21 17:49:12 +0900

    return list challenges

--- src/app/handler.c
+++ src/app/handler.c
@@ -1,6 +1,10 @@
 #include "handler.h"
 
 #include <assert.h>
+#include <stddef.h>
+#include <stdio.h>
+
+#include "json.h"
 
 bool
 handle_root(struct http_request_context *ctx, db_pool_t *db, db_task_t *task, http_response_t *out_response)
@@ -48,5 +52,70 @@ handle_get_challenges_1(struct http_request_context *ctx, db_pool_t *db, db_task
 bool
 handle_get_challenges_2(struct http_request_context *ctx, db_pool_t *db, db_task_t *task, http_response_t *out_response)
 {
+  assert(ctx->current_handler->next == NULL);
+  assert(task != NULL);
+  assert(task->result != NULL);
+
+  if (!task->result->success)
+  {
+    *out_response = (http_response_t){
+        .status = HTTP_STATUS_INTERNAL_SERVER_ERROR,
+        .body = "db error",
+        .body_len = 8,
+    };
+    fprintf(stderr, "Database query failed: %s\n", task->result->err_msg);
+    return true;
+  }
+
+  MYSQL_RES *res = task->result->res;
+  uint64_t rows = mysql_num_rows(res);
+  if (rows == 0)
+  {
+    *out_response = (http_response_t){
+        .status = HTTP_STATUS_OK,
+        .body = "[]",
+        .body_len = 2,
+    };
+    mysql_free_result(res);
+    return true;
+  }
+
+  challenge_t *challenges = bind_challenges(task->result);
+  if (!challenges)
+  {
+    *out_response = (http_response_t){
+        .status = HTTP_STATUS_INTERNAL_SERVER_ERROR,
+        .body = "db error",
+        .body_len = 8,
+    };
+    mysql_free_result(res);
+    fprintf(stderr, "Failed to bind challenges from database result\n");
+    return true;
+  }
+
+  string_t json_str;
+  challenges_to_json(challenges, rows, &json_str, false);
+  if (!json_str.ptr)
+  {
+    *out_response = (http_response_t){
+        .status = HTTP_STATUS_INTERNAL_SERVER_ERROR,
+        .body = "json error",
+        .body_len = 10,
+    };
+    fprintf(stderr, "Failed to convert challenges to JSON\n");
+    mysql_free_result(res);
+    free(challenges);
+    return true;
+  }
+
+  *out_response = (http_response_t){
+      .status = HTTP_STATUS_OK,
+      .body = json_str.ptr,
+      .body_len = json_str.len,
+  };
+
+  mysql_free_result(res);
+  free(challenges);
+
   return true;
 }
