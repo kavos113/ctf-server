7ad0b5a2648ee3368e8a8245ddb401bd7d6d79ad
kavos113 <kohei.hadano@gmail.com>
2026-09-22 23:46:25 +0900

    show creator

--- client/src/pages/detail/detail-page.ts
+++ client/src/pages/detail/detail-page.ts
@@ -1,4 +1,4 @@
-import type { Answer, CorrectAnswer, PublicChallenge } from '../../api/api';
+import type { Answer, CorrectAnswer, PublicChallenge, User } from '../../api/api';
 import { errorMessage } from '../../api/http-client';
 import { PageState, formatDate, parseId } from '../shared/page-state';
 import './detail-page.css';
@@ -6,6 +6,7 @@ import './detail-page.css';
 export class DetailPage extends PageState {
   id?: number;
   challenge?: PublicChallenge;
+  users: User[] = [];
   answer = '';
   own: Answer[] = [];
   publicAnswers: CorrectAnswer[] = [];
@@ -13,6 +14,12 @@ export class DetailPage extends PageState {
   private submittedCorrectly = false;
   private historyGeneration = 0;
   formatDate = formatDate;
+
+  get creatorName() {
+    const id = this.challenge?.creator_id;
+
+    return id ? this.users.find((user) => user.id === id)?.username || id : '作成者不明';
+  }
 
   get solved() {
     return (
@@ -40,6 +47,7 @@ export class DetailPage extends PageState {
 
   async refresh() {
     this.challenge = undefined;
+    this.users = [];
     this.own = [];
     this.publicAnswers = [];
     this.historyError = '';
@@ -51,9 +59,10 @@ export class DetailPage extends PageState {
     }
 
     await this.read(
-      () => this.api.challenges(),
-      (items) => {
+      () => Promise.all([this.api.challenges(), this.api.users().catch(() => [])]),
+      ([items, users]) => {
         this.challenge = items.find((item) => item.id === this.id);
+        this.users = users;
 
         if (!this.challenge) {
           this.error = '問題が見つかりません。';
