13eeccc0b1d02e371b6f868e30c9f9593d1f22c7
kavos113 <kohei.hadano@gmail.com>
2026-09-22 18:01:21 +0900

    query param

--- src/db.c
+++ src/db.c
@@ -2,6 +2,7 @@
 
 #include "db.h"
 
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -30,6 +31,10 @@ task_queue_free(task_queue_t *queue)
   while (queue->head)
   {
     db_task_t *top = task_queue_pop(queue);
+    if (top->is_param_query)
+    {
+      free(top->result);
+    }
     free(top);
   }
 
@@ -49,7 +54,7 @@ task_queue_push(task_queue_t *queue, db_task_t *task)
   }
   else
   {
-    queue->head = queue->head = task;
+    queue->head = queue->tail = task;
   }
 
   pthread_cond_signal(&queue->cond);
@@ -84,6 +89,97 @@ task_queue_pop(task_queue_t *queue)
   return task;
 }
 
+static void
+set_param_query_error(db_result_t *result, const char *message)
+{
+  snprintf(result->err_msg, sizeof(result->err_msg), "%s", message);
+  result->err_msg_len = strlen(result->err_msg);
+}
+
+static void
+execute_param_query(MYSQL *conn, db_task_t *task)
+{
+  db_result_t *result = task->result;
+  MYSQL_STMT *stmt = mysql_stmt_init(conn);
+  if (!stmt)
+  {
+    set_param_query_error(result, "cannot allocate statement");
+    return;
+  }
+
+  MYSQL_BIND *bindings = NULL;
+  if (mysql_stmt_prepare(stmt, task->query, task->query_len) != 0)
+  {
+    set_param_query_error(result, mysql_stmt_error(stmt));
+    goto done;
+  }
+
+  if (mysql_stmt_field_count(stmt) != 0)
+  {
+    set_param_query_error(result, "queries returning rows are not supported");
+    goto done;
+  }
+
+  if (mysql_stmt_param_count(stmt) != task->param_count)
+  {
+    set_param_query_error(result, "parameter count mismatch");
+    goto done;
+  }
+
+  if (task->param_count)
+  {
+    bindings = calloc(task->param_count, sizeof(*bindings));
+    if (!bindings)
+    {
+      set_param_query_error(result, "cannot allocate parameter bindings");
+      goto done;
+    }
+
+    for (size_t i = 0; i < task->param_count; i++)
+    {
+      db_param_t *param = &task->params[i];
+      switch (param->type)
+      {
+      case DB_PARAM_STRING:
+        bindings[i].buffer_type = MYSQL_TYPE_STRING;
+        bindings[i].buffer = (void *)param->value.string.ptr;
+        bindings[i].buffer_length = param->value.string.len;
+        bindings[i].length = &bindings[i].buffer_length;
+        break;
+
+      case DB_PARAM_INT64:
+        bindings[i].buffer_type = MYSQL_TYPE_LONGLONG;
+        bindings[i].buffer = &param->value.integer;
+        bindings[i].buffer_length = sizeof(param->value.integer);
+        break;
+
+      case DB_PARAM_NULL:
+        bindings[i].buffer_type = MYSQL_TYPE_NULL;
+        break;
+      }
+    }
+    if (mysql_stmt_bind_param(stmt, bindings) != 0)
+    {
+      set_param_query_error(result, mysql_stmt_error(stmt));
+      goto done;
+    }
+  }
+
+  if (mysql_stmt_execute(stmt) != 0)
+  {
+    set_param_query_error(result, mysql_stmt_error(stmt));
+    goto done;
+  }
+
+  result->success = 1;
+  result->affected = mysql_stmt_affected_rows(stmt);
+  result->insert_id = mysql_stmt_insert_id(stmt);
+
+done:
+  mysql_stmt_close(stmt);
+  free(bindings);
+}
+
 void *
 db_worker_thread(void *arg)
 {
@@ -113,23 +209,30 @@ db_worker_thread(void *arg)
       break;
     }
 
-    int err = mysql_query(conn, task->query);
-    task->result = calloc(1, sizeof(db_result_t));
-    if (err == 0)
+    if (task->is_param_query)
     {
-      MYSQL_RES *res = mysql_store_result(conn);
-      task->result->res = res;
-      task->result->success = 1;
+      execute_param_query(conn, task);
     }
     else
     {
-      int len = snprintf(task->result->err_msg, sizeof(task->result->err_msg), "error: %s", mysql_error(conn));
-      task->result->err_msg_len = len;
-      task->result->success = 0;
-    }
+      int err = mysql_query(conn, task->query);
+      task->result = calloc(1, sizeof(db_result_t));
+      if (err == 0)
+      {
+        MYSQL_RES *res = mysql_store_result(conn);
+        task->result->res = res;
+        task->result->success = 1;
+      }
+      else
+      {
+        int len = snprintf(task->result->err_msg, sizeof(task->result->err_msg), "error: %s", mysql_error(conn));
+        task->result->err_msg_len = len;
+        task->result->success = 0;
+      }
 
-    my_ulonglong affected = mysql_affected_rows(conn);
-    task->result->affected = affected;
+      my_ulonglong affected = mysql_affected_rows(conn);
+      task->result->affected = affected;
+    }
 
     task_queue_push(pool->done_queue, task);
 
@@ -252,6 +355,79 @@ db_pool_exec_query(db_pool_t *pool, const char *query, size_t query_len, void *d
   task->data = data;
 
   task_queue_push(pool->task_queue, task);
+}
+
+int
+db_exec_query_param(db_pool_t *pool, const char *query, size_t query_len,
+                    const db_param_t *params, size_t param_count, void *data)
+{
+  if (!pool || !pool->task_queue || !query || query_len == 0 || query_len >= DEFAULT_QUERY_SIZE || memchr(query, '\0', query_len) || (param_count && !params))
+  {
+    return -1;
+  }
+  size_t size = sizeof(db_task_t) + param_count * sizeof(db_param_t);
+  for (size_t i = 0; i < param_count; i++)
+  {
+    switch (params[i].type)
+    {
+    case DB_PARAM_STRING:
+    {
+      size_t len = params[i].value.string.len;
+      if ((!params[i].value.string.ptr && len) || len > ULONG_MAX || len >= SIZE_MAX - size)
+      {
+        return -1;
+      }
+      size += len + 1;
+      break;
+    }
+
+    case DB_PARAM_INT64:
+    case DB_PARAM_NULL:
+      break;
+    default:
+      return -1;
+    }
+  }
+
+  db_task_t *task = calloc(1, size);
+  if (!task)
+  {
+    return -1;
+  }
+
+  // Allocate the result before enqueueing so allocation failure is synchronous.
+  task->result = calloc(1, sizeof(db_result_t));
+  if (!task->result)
+  {
+    free(task);
+    return -1;
+  }
+
+  memcpy(task->query, query, query_len);
+  task->query_len = query_len;
+  task->is_param_query = 1;
+  task->param_count = param_count;
+  task->data = data;
+
+  char *strings = (char *)(task->params + param_count);
+  for (size_t i = 0; i < param_count; i++)
+  {
+    task->params[i] = params[i];
+    if (params[i].type == DB_PARAM_STRING)
+    {
+      size_t len = params[i].value.string.len;
+      if (len)
+      {
+        memcpy(strings, params[i].value.string.ptr, len);
+      }
+      strings[len] = '\0';
+      task->params[i].value.string.ptr = strings;
+      strings += len + 1;
+    }
+  }
+  
+  task_queue_push(pool->task_queue, task);
+  return 0;
 }
 
 db_task_t *
--- src/db.h
+++ src/db.h
@@ -13,10 +13,32 @@ typedef struct
   int success;
   MYSQL_RES *res;
   uint64_t affected;
+  uint64_t insert_id;
 
   char err_msg[256];
   int err_msg_len;
 } db_result_t;
+
+typedef enum
+{
+  DB_PARAM_STRING,
+  DB_PARAM_INT64,
+  DB_PARAM_NULL,
+} db_param_type;
+
+typedef struct
+{
+  db_param_type type;
+  union
+  {
+    struct
+    {
+      const char *ptr;
+      size_t len;
+    } string;
+    int64_t integer;
+  } value;
+} db_param_t;
 
 typedef struct db_task
 {
@@ -28,6 +50,11 @@ typedef struct db_task
   void *data;
 
   struct db_task *next;
+
+  int is_param_query;
+  size_t query_len;
+  size_t param_count;
+  db_param_t params[];
 } db_task_t;
 
 typedef struct
@@ -76,6 +103,9 @@ db_pool_t *db_pool_new_from_env(int epoll_fd, int num_threads);
 void db_pool_free(db_pool_t *pool);
 
 void db_pool_exec_query(db_pool_t *pool, const char *query, size_t query_len, void *data);
+int db_exec_query_param(db_pool_t *pool, const char *query, size_t query_len,
+                        const db_param_t *params, size_t param_count, void *data);
+
 db_task_t *db_pool_get_latest_completed_task(db_pool_t *pool);
 
 #endif // MYSQL_H
